e58b28ae751f05254c405dd6e6c2f498d4c0078b
HamdyIIbrahim <hamdyfarouk444@gmail.com>
2023-05-16 15:45:46 +0300

    add url to mission schema

--- models/mission.js
+++ models/mission.js
@@ -4,9 +4,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 const missionSchema = mongoose.Schema({
   grade: { type: String, required: true, unique: true },
   missions: {
-    planet1: {type:[String]},
-    planet2: {type:[String]},
-    planet3: {type:[String]},
+    planet1: {type:[{NAME:{type:String},URL:{type:String}}]},
+    planet2: {type:[{NAME:{type:String},URL:{type:String}}]},
+    planet3: {type:[{NAME:{type:String},URL:{type:String}}]},
   },
 });
 
